f4b673e88eb63cc24953a036dbddb962d1446be5
contabilidad-spec <contabilidad@reddyset.co>
2025-10-16 08:27:47 -0500

    Update zoho.js

--- api/zoho.js
+++ api/zoho.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
 
   const response = await fetch("https://www.zohoapis.com/books/v3/invoices?organization_id=822181064", {
     headers: {
-      "Authorization": "Zoho-oauthtoken 1000.345eafc7218ef67518600db4b8c24ec8.51f3099eadee735466772f1f3fedcf54",
+      "Authorization": "Zoho-oauthtoken 1000.1a0d892a6f6a30ead3f57c1a943637aa.33213692e6ee33776449a5de98b2b2f0",
       "X-com-zoho-organizationid": "822181064",
       "Content-Type": "application/json"
     }
@@ -12,4 +12,5 @@ export default async function handler(req, res) {
   const data = await response.json();
   res.status(200).json(data);
 }
+
 
